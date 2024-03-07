4aca9323cde569a061624f4ef5b66d524cadcdf5
Marco6792 <marykang400@gmail.com>
2024-03-07 02:43:22 +0100

    slate

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -29,7 +29,7 @@ export default function BlogPost() {
 					<div>
 						<CardTitle className="dark:text-slate-200 mt-[6rem] ">
 							<span className="dark:bg-slate-800 rounded-[50px] w-fit p-4 bg-gray-100 border">
-								1.1 BACKGROUND
+								<span className="text-slate-700">1.1</span>BACKGROUND
 							</span>
 						</CardTitle>
 						<Separator orientation="vertical" className="h-[2rem] ml-5"></Separator>
